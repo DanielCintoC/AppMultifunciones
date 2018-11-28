2ccb7a3569452960f330d5863346fdd02739a22a
Daniel Cinto <control_why_you@hotmail.com>
2018-11-28 10:32:17 -0600

    Initial Commit

---
+++ Coding/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Coding
+//
+//  Created by daniel cinto conde on 28/11/18.
+//  Copyright © 2018 daniel cinto conde. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
