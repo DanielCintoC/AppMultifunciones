cd35b2f21485d76e9c51614e7a3e0c121d283e59
Daniel Cinto <control_why_you@hotmail.com>
2018-12-10 22:13:44 -0600

    Los segues y transiciones

---
+++ Coding/Controllers/FontDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FontDetailViewController.swift
+//  Coding
+//
+//  Created by daniel cinto conde on 10/12/18.
+//  Copyright © 2018 daniel cinto conde. All rights reserved.
+//
+
+import UIKit
+
+class FontDetailViewController: UIViewController {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var pickerFonts: UIPickerView!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- Coding/Controllers/ThirdViewController.swift
+++ Coding/Controllers/ThirdViewController.swift
@@ -8,23 +8,51 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
-
+class ThirdViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var families: [String] = []
+    var fonts: [String: [String]] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        families = UIFont.familyNames.sorted( by: { return $0 < $1 } )
+        
+        for fam in families {
+            fonts[fam] = UIFont.fontNames(forFamilyName: fam)
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Métodos del protocolo UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    // Cuantas filas vas a tener en cada sección
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.families.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FontFamilyCell", for: indexPath)
+        let fontFamily = self.families[indexPath.row]
+        cell.textLabel?.text = fontFamily
+        cell.textLabel?.font = UIFont(name: fontFamily, size: 20.0)
+        return cell
+    }
+    
+    //MARK: - Métodos del protocolo de UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let row = indexPath.row
+        let fontFamily = self.families[row]
+        let familyFonts = fonts[fontFamily]!
+        
+    }
 
 }
